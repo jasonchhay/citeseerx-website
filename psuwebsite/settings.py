7619ba84934b1d4f4b7b84312c196e5cdc82de3a
jxc743 <jefferycao2016@gmail.com>
2017-12-21 22:05:57 -0500

    Added static files for homepage

--- psuwebsite/settings.py
+++ psuwebsite/settings.py
@@ -118,4 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_DIRS = (
+    'home/PycharmProjects/psuwebsite/psuwebsite/static',
+    os.path.join(BASE_DIR, 'psuwebsite/static/'),
+)
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
