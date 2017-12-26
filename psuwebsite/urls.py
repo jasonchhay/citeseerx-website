33731a4e6f9b0335beb96bdf2b0f848d0f2eed48
jxc743 <jefferycao2016@gmail.com>
2017-12-25 20:43:08 -0500

    Add a database for people. One database, but professor, postdoc, phd, masters, undergraduates, staff categories.

--- psuwebsite/urls.py
+++ psuwebsite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +27,8 @@ urlpatterns = [
 
     path('home', views.index, name = 'home'),
 
-    path('people', views.people, name = 'people'),
-]
+    path('people/', include('people.urls')),
+
+
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
--- psuwebsite/views.py
+++ psuwebsite/views.py
@@ -4,6 +4,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'psuwebsite/index.html')
-
-def people(request):
-    return render(request, 'psuwebsite/people.html')
