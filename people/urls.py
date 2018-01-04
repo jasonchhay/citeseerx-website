be3c038eaad27431ee176294259963ed924522c4
jxc743 <jefferycao2016@gmail.com>
2018-01-04 17:46:41 -0500

    Finished the people page, next add search function to publications and make blog/projects pages.

--- people/urls.py
+++ people/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.people, name = 'people')
+    path('', views.people, name = 'people'),
+
+    path('professors', views.professors, name = 'professors'),
+
+    path('PostDocs', views.postdocs, name = 'PostDocs'),
+
+    path('Phd_students', views.phdstudents, name = 'Phd_students'),
+
+    path('Master', views.master, name = 'master'),
+
+    path('Undergraduates', views.undergraduate, name = 'undergraduates'),
+
+    path('Staff', views.staff, name = 'staff'),
+
+    path('Alumni', views.alumni1, name='alumni')
+
+
 ]
--- people/views.py
+++ people/views.py
@@ -28,3 +28,40 @@ def people(request):
                                                       'mastersalumni': masteralumni,
                                                       'staffsalumni': staffalumni
                                                       })
+
+
+def professors(request):
+    professors = Professor.objects.all().filter().order_by('id')
+    return render(request, 'psuwebsite/professors.html', {'professors': professors})
+
+def postdocs(request):
+    postdoc = CurrentTeam.objects.all().filter(occupation = 'Postdoc')
+    return render(request, 'psuwebsite/postdoc.html', {'postdocs': postdoc})
+
+def phdstudents(request):
+    phdstudent = CurrentTeam.objects.all().filter(occupation = 'PhD Student')
+    return render(request, 'psuwebsite/phdstudent.html', {'phdstudents': phdstudent})
+
+def master(request):
+    master = CurrentTeam.objects.all().filter(occupation = 'Masters Student')
+    return render(request, 'psuwebsite/master.html', {'masters': master})
+
+def undergraduate(request):
+    undergraduate = CurrentTeam.objects.all().filter(occupation='Undergraduate')
+    return render(request, 'psuwebsite/undergraduate.html', {'undergraduates': undergraduate})
+
+def staff(request):
+    staff = CurrentTeam.objects.all().filter(occupation = 'Staff')
+    return render(request, 'psuwebsite/staff.html', {'staffs': staff})
+
+def alumni1(request):
+    undergraduatealumni = alumni.objects.all().filter(occupation = "Undergraduate")
+    postdocalumni = alumni.objects.all().filter(occupation='Postdoc')
+    phdstudentalumni = alumni.objects.all().filter(occupation='PhD Student')
+    masteralumni = alumni.objects.all().filter(occupation='Masters Student')
+    staffalumni = alumni.objects.all().filter(occupation='Staff')
+    return render(request, 'psuwebsite/alumni.html', {'undergraduatesalumni': undergraduatealumni,
+                                                      'postdocsalumni': postdocalumni,
+                                                      'phdstudentsalumni': phdstudentalumni,
+                                                      'mastersalumni': masteralumni,
+                                                      'staffsalumni': staffalumni})
